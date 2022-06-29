79a5da017bd149a7829795a7065f62ea73df2bcb
Krishnendu-Banerjee <74444809+Krishnendu-Banerjee@users.noreply.github.com>
2022-06-29 22:43:13 +0530

    Add files via upload

---
+++ test code apple.py
@@ -0,0 +1,110 @@
+import colorama
+import time
+from colorama import Fore, Back, init
+class colors:
+    iosText = '\033[95m'
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    lime = '\033[92m'
+    war = '\033[93m'
+    uns = '\033[91m'
+    ENDC = '\033[0m'
+    bold = '\033[1m'
+    und = '\033[4m'
+time.sleep(1)
+colorama.init(convert=True)
+print(Fore.GREEN+'Welcome to APPLE calculator (:')
+time.sleep(1)
+print(".")
+time.sleep(1)
+
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(1)
+
+print(".")
+time.sleep(0.5)
+d=(input("Please enter your credit card number : "))
+if d=="for what?":
+    time.sleep(0.5)
+    print("business policy ;)")
+time.sleep(0.5)
+d=(input("Please enter your credit card number : "))
+time.sleep(0.3)
+a=(input("BTW..Are you broke? : "))
+time.sleep(0.5)
+if a=="yes":
+    time.sleep(0.5)
+    print("It's okay.. it won't cost you much (:")
+    b=str(input())
+    if b=="cost??":
+        time.sleep(0.3)
+        print("eh")
+time.sleep(0.5)
+print("Anyways..")
+time.sleep(0.5)
+c=(input("What calculation do you want to do?-> "))
+if c=="exponential":
+    time.sleep(0.5)
+    print("Okay")
+    time.sleep(0.5)
+print("Exponential would cost you 2£")
+time.sleep(0.3)
+e=(input("Proceed to payment? (Yes/No): "))
+if e=="Nooo":
+    time.sleep(0.5)
+    f=(input("Why? "))
+    time.sleep(0.3)
+    print("Dont worry. We give 100% accurate answers :)")
+    time.sleep(0.5)
+g=(input("Proceed to payment? (Y/N): "))
+if g=="umm alright":
+    time.sleep(0.3)
+    print("2£ has been successfully paid")
+h=(input())
+time.sleep(0.3)
+print("Sorry for the delay")
+time.sleep(0.3)
+i=int(input("What base would you want to use : "))
+time.sleep(0.3)
+j=int(input("What will be the power : "))
+exp=i**j
+time.sleep(0.2)
+print("Oh I forgot to mention one thing")
+time.sleep(0.5)
+print("For two digit powers you have to pay 0.2£")
+k=(input())
+time.sleep(0.2)
+l=(input("Proceed(Y/N): "))
+time.sleep(0.2)
+print("Your answer is ", exp)
+m=(input())
+time.sleep(0.5)
+print("WAIT")
+n=(input())
+time.sleep(0.3)
+print("For 4 digit answers you have to pay 400£")
+o=(input())
+time.sleep(1)
+print("We have already given the answer. You have no choice")
+time.sleep(3)
+print("nice try")
+time.sleep(2)
+print("You cant end this program!")
+time.sleep(4)
+print("_MICROSOFT JOINED THE CODE_")
+time.sleep(3)
+print("$RemoveApple$")
+time.sleep(1)
+print("_Apple was removed from the code_")
+P=(input())
+time.sleep(0.3)
+print("It's aight")
+q=(input())
+time.sleep(2)
+print("Not so fast")
+r=(input())
+time.sleep(0.3)
+print("First gimmie a calculation")
+time.sleep(5)
